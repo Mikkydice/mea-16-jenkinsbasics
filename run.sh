f7164fb3810dbac54df0701bce5351de33ca81e4
Destiny <Oibohdestiny4@gmail.com>
2023-12-05 10:36:04 +0000

    added zip and txt

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-echo "Hello world"
-echo "Now trying webhooks"
-echo "hope it works"
-echo "Thank you"
+echo "Hello world" > 1.txt
+echo "Now trying webhooks" > 2.txt
+echo "hope it works" > 3.txt
+echo "Thank you" > 4.txt
+
+
+zip archive.zip *.txt 
